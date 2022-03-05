29113f25afbc9d83db564962f10d60877910f8ae
lohit123568 <87901963+lohit123568@users.noreply.github.com>
2022-03-05 16:39:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser(){
+    player1_name_input = document.getElementById("player1_name_input").value;
+    player2_name_input = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name_input",player1_name_input);
+    localStorage.setItem("player2_name_input",player2_name_input); 
+    window.location = "quiz_game.html";
+}
